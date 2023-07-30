b2d0c08d7bd4f028c321572feb07dfd7a3686943
radss <fajfrradek@seznam.cz>
2023-07-30 16:16:32 +0200

    comment

--- 1Z0-808-Examples/src/ce/examples/chapter6/ClownFish.java
+++ 1Z0-808-Examples/src/ce/examples/chapter6/ClownFish.java
@@ -8,7 +8,7 @@ package ce.examples.chapter6;
 	 //vlastní overloadnutá metoda, žádný problém
 	 public String getNumberOfGills() { return "4"; }
 	 //zde chce overridnout (stejný poèet args i název) ale jiný návratový typ
-	 public String getNumberOfGills(int input) { return "6"; }
+	 //public String getNumberOfGills(int input) { return "6"; }
 	 
 	 public static void main(String[] args) {
 		 System.out.println(new ClownFish().getNumberOfGills(-1));
